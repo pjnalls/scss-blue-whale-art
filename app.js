2972f5bb3ae4b811607a26c6849e4661f54b7772
pjnalls <pjnalls.js@gmail.com>
2022-01-03 19:19:40 -0600

    Add and append `.mouth` element to `.canvas`.

--- app.js
+++ app.js
@@ -8,11 +8,13 @@ const main = () => {
   const root = document.getElementById("root"),
     canvas = createDiv("canvas"),
     body = createDiv("body"),
-    sideFin = createDiv("side-fin");
+    sideFin = createDiv("side-fin"),
+    mouth = createDiv("mouth");
 
   canvas.appendChild(body);
   canvas.appendChild(sideFin);
-  
+  canvas.appendChild(mouth);
+
   root.appendChild(canvas);
 };
 
