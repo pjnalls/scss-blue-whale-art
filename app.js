c238cbac12eb92427fc1b803b178395517cdcd70
pjnalls <pjnalls.js@gmail.com>
2022-01-03 18:28:14 -0600

    Add `createDiv` and `main` functions.

---
+++ app.js
@@ -0,0 +1,14 @@
+const createDiv = (className) => {
+  const div = document.createElement("div");
+  div.setAttribute("class", className);
+  return div;
+};
+
+const main = () => {
+  const root = document.getElementById("root"),
+    canvas = createDiv("canvas");
+
+  root.appendChild(canvas);
+};
+
+main();
